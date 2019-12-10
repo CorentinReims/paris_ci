3f5f25796311d2b6505d9054966a97ef63ce3da4
Felix HENG <flxheng@gmail.com>
2019-12-10 13:42:20 +0100

    resolve error

--- scripts/deploy_production_app.sh
+++ scripts/deploy_production_app.sh
@@ -1,3 +1,3 @@
-echo "{ "alias": "production-corentinci" }" > now-production-app.json
+echo "{ \"alias\": \"production-corentinci\" }" > now-production-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-production-app.json
